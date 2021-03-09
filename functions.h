8865e106f4189320caef9103f76485c84de8612d
Maudeline Deus <mdeus1@ugrad3.cs.jhu.edu>
2021-03-09 14:38:53 -0500

    minor edits

---
+++ functions.h
@@ -0,0 +1,4 @@
+#include <iostream>
+
+int lru();
+int fifo();
--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 #include "functions.h"
 
 int main (int argc, char *argv[]) {
+    if(argc != 7) {
+        printf("ERROR: not enough arguments\n");
+        return 1;
+    }
 
+
+    return 0;
 }
